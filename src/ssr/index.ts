adc90b5258333f680737fa2916b72edcc543e1b6
filippovdima5 <filippovdima5yandex.ru>
2020-03-31 23:02:24 +0300

    node env

--- src/ssr/index.ts
+++ src/ssr/index.ts
@@ -18,19 +18,21 @@ const cacheRender = createCache(renderLRU)
 const app = new Koa()
 
 const setupKoa = () => {
-  app.use(mount('/static', serve(`${path.resolve(__dirname, 'static')}`)))
-  app.use(mount('/cdn', serve(`${path.resolve(__dirname, 'cdn')}`)))
-  app.use(mount('/icons', serve(`${path.resolve(__dirname, 'icons')}`)))
-  app.use(mount('/favicon', serve(`${path.resolve(__dirname, '/')}`)))
-  app.use(mount('/loadable-stats', serve(`${path.resolve(__dirname, '/')}`)))
-  app.use(mount('/precache-manifest', serve(`${path.resolve(__dirname, '/')}`)))
-  
-  app.use(mount('/api', proxy({
-    host: 'http://localhost:8080',
-    map: path => `/api${path}`
-  })))
-  
-  app.use(logger())
+  if (process.env.NODE_ENV !== 'production') {
+    app.use(mount('/static', serve(`${path.resolve(__dirname, 'static')}`)))
+    app.use(mount('/cdn', serve(`${path.resolve(__dirname, 'cdn')}`)))
+    app.use(mount('/icons', serve(`${path.resolve(__dirname, 'icons')}`)))
+    app.use(mount('/favicon', serve(`${path.resolve(__dirname, '/')}`)))
+    app.use(mount('/loadable-stats', serve(`${path.resolve(__dirname, '/')}`)))
+    app.use(mount('/precache-manifest', serve(`${path.resolve(__dirname, '/')}`)))
+    
+    app.use(mount('/api', proxy({
+      host: 'http://localhost:8080',
+      map: path => `/api${path}`
+    })))
+    
+    app.use(logger())
+  }
   
   app.use(async (ctx) => {
     let sexIdUser: 1 | 2 | null = null
