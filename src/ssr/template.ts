9491b4a04d3aec928fedbba34e802e11aeb44c83
filippovdima5 <filippovdima5yandex.ru>
2020-04-01 19:25:29 +0300

    yandex metrika

--- src/ssr/template.ts
+++ src/ssr/template.ts
@@ -25,6 +25,26 @@ export const template = (
     <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
     <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#818890">
     ${styleTags}
+    
+    
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(53255053, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+        });
+    </script>
+    
+    <noscript><div><img src="https://mc.yandex.ru/watch/53255053" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+    
+    
 </head>
 
 <body>
