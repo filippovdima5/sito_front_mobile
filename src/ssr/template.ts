06d46ef91fe30cd257fa4eb7edcf7810417ebc80
filippovdima5 <filippovdima5yandex.ru>
2020-04-02 18:30:39 +0300

    ppixel mail

--- src/ssr/template.ts
+++ src/ssr/template.ts
@@ -80,6 +80,22 @@ export const template = (
 </head>
 
 <body>
+    
+    <!— Rating Mail.ru counter —>
+        <script type="text/javascript">
+            var _tmr = window._tmr || (window._tmr = []);
+            _tmr.push({id: "3169578", type: "pageView", start: (new Date()).getTime()});
+            (function (d, w, id) {
+            if (d.getElementById(id)) return;
+            var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+            ts.src = "https://top-fwz1.mail.ru/js/code.js";
+            var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+            if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+            })(document, window, "topmailru-code");
+        </script>
+        <noscript><div><img src="https://top-fwz1.mail.ru/counter?id=3169578;js=na" style="border:0;position:absolute;left:-9999px;" alt="Top.Mail.Ru" /></div></noscript>
+    <!— //Rating Mail.ru counter —>
+    
     <noscript>Пожалуйста, включите JavaScript в настройках браузера для корректного отображения контента сайта.</noscript>
     <script>window.INITIAL_STATE = ${JSON.stringify(preloadedState)}</script>
     ${scripts}
