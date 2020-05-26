6271d8113791d39451f99c8a604fdd1a20a3298a
Dmitry <39603825+filippovdima5@users.noreply.github.com>
2020-05-26 18:31:12 +0300

    fix

--- src/helpers/lib/index.ts
+++ src/helpers/lib/index.ts
@@ -1,14 +1,14 @@
-export const sortRecord = function sortRecord <T>(obj: T): T {
-  return (
-    Object.fromEntries(
-      Object.entries(obj).sort((a: any, b: any) => (a[0] as any - b[0] as any))
-    )
-  )
-}
+// export const sortRecord = function sortRecord <T>(obj: T): T {
+//   return (
+//     Object.fromEntries(
+//       Object.entries(obj).sort((a: any, b: any) => (a[0] as any - b[0] as any))
+//     )
+//   )
+// }
 
-export const compareRecord = function compareRecord (obj1: any, obj2: any): boolean {
-  return (JSON.stringify(sortRecord(obj1)) === JSON.stringify(sortRecord(obj2)))
-}
+// export const compareRecord = function compareRecord (obj1: any, obj2: any): boolean {
+//   return (JSON.stringify(sortRecord(obj1)) === JSON.stringify(sortRecord(obj2)))
+// }
 
 export const sexIdToStr = function sexIdToStr (sexId: 1 | 2 | 0): 'men' | 'women' {
   if (sexId === 1) return 'men'
