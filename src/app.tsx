3f4c560bfe06e1e963bf4baeccc8eaad0cd43bda
Dmitry <39603825+filippovdima5@users.noreply.github.com>
2020-06-11 01:29:06 +0300

    ddd

--- src/app.tsx
+++ src/app.tsx
@@ -4,9 +4,15 @@ import { Provider } from 'effector-react/ssr'
 import { AppTemplate } from './features/app-template'
 import { ListenLocation } from './features/listen-location'
 import { MetaTags } from './features/meta-tags'
+import { useEffectSafe } from './hooks/use-effect-safe'
+import { polyfills } from './polyfills'
 
 
 export function App({ root }: { root: Scope }) {
+  useEffectSafe(() => {
+    polyfills()
+  }, [])
+  
   return(
     <Provider value={root}>
       <ListenLocation/>
