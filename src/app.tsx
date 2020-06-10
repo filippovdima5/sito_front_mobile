b1fdf8f72f51895d645c061fe3f3c34b2d1da56e
Dmitry <39603825+filippovdima5@users.noreply.github.com>
2020-06-11 01:25:11 +0300

    sss

--- src/app.tsx
+++ src/app.tsx
@@ -4,10 +4,7 @@ import { Provider } from 'effector-react/ssr'
 import { AppTemplate } from './features/app-template'
 import { ListenLocation } from './features/listen-location'
 import { MetaTags } from './features/meta-tags'
-import { polyfills } from './polyfills'
 
-
-polyfills()
 
 export function App({ root }: { root: Scope }) {
   return(
